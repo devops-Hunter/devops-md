fe5219305a90ae3e1389585972e272e083f1a020
苏祥凤 <suxiangfeng@ebaochina.cn>
2021-04-07 11:03:21 +0800

    feat:添加getVersion api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as ready } from './ready';
 export { default as chooseImage } from './chooseImage';
 export { default as previewImage } from './previewImage';
+export { default as getVersion } from './version';
